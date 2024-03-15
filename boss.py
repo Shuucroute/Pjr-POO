fb507987414c9dabb5f5de78ed753b29b6723bce
Shuucroute <ethan.rampnoux@gmail.com>
2024-03-15 09:14:04 +0100

    Création classe Boss et modification classe ennemis

---
+++ boss.py
@@ -0,0 +1,82 @@
+from character import Character
+from dice import Dice
+
+
+
+class Boss(Character):
+    boss_killed_count = 0
+    def __init__ (self, name="Boss", hp=50, attack_value=20, defense_value=20, dice=Dice("Red", 10)):
+        return super().__init__(name, hp, attack_value, defense_value, dice)
+
+class Cadaverus_Devorator(Boss): #Boss Zombie
+    def compute_damages(self, roll, target):
+        print("Cadaverus Devorator vous attaque ! (+5 dmg)")
+        return super().compute_damages(roll, target) + 5
+    
+    def compute_raw_damages(self, damages, roll, attacker):
+        print("Cadaverus Devorator prends des dégâts ! (-5 dmg)")
+        raw_damages = super().compute_raw_damages(damages, roll, attacker) - 5
+        return max(0, raw_damages)
+    
+    @classmethod
+    def create_boss(cls, dice):
+        return cls(name="Cadaverus Devorator",dice=dice)
+
+class Kondylos_o_Sarantapus(Boss): #boss squelette
+    def compute_damages(self, roll, target):
+        print("Le Kondylos o Sarantapus vous attaque ! (+6 dmg)")
+        return super().compute_damages(roll, target) + 6
+    
+    def compute_raw_damages(self, damages, roll, attacker):
+        print("Le ondylos o Sarantapus prends des dégâts ! (-4 dmg)")
+        raw_damages = super().compute_raw_damages(damages, roll, attacker) - 4
+        return max(0, raw_damages)
+    
+    @classmethod
+    def create_boss(cls, dice):
+        return cls(name="Kondylos o Sarantapus",dice=dice)
+    
+class Roi_Gobelin(Boss): #boss gobelin
+    def compute_damages(self, roll, target):
+        print("Le Roi Gobelin vous attaque ! (+7 dmg)")
+        return super().compute_damages(roll, target) + 7
+    
+    def compute_raw_damages(self, damages, roll, attacker):
+        print("Le Roi Gobelin prends des dégâts ! (-7 dmg)")
+        raw_damages = super().compute_raw_damages(damages, roll, attacker) - 7
+        return max(0, raw_damages)
+    
+    @classmethod
+    def create_boss(cls, dice):
+        return cls(name="Roi Gobelin",dice=dice)
+    
+class Garrok_le_Féroce(Boss): #troll boss
+    def compute_damages(self, roll, target):
+        print("Le Garrok le Féroce vous attaque ! (+10 dmg)")
+        return super().compute_damages(roll, target) +10 
+    
+    def compute_raw_damages(self, damages, roll, attacker):
+        print("Le Garrok le Féroce prends des dégâts ! (-10 dmg)")
+        raw_damages = super().compute_raw_damages(damages, roll, attacker) -10 
+        return max(0, raw_damages)
+    
+    @classmethod
+    def create_boss(cls, dice):
+        return cls(name="Garrok le Féroce",dice=dice)
+
+    #@classmethod
+    #def increase_boss_killed_count(cls):
+        #cls.boss_killed_count += 1
+        #if cls.boss_killed_count % 5 == 0:
+            #return Balrog.summon_balrog()
+        #else:
+            #return None
+
+class Balrog(Character):
+    def __init__ (self, name="Balrog", hp=100, attack_value=50, defense_value=50, dice=Dice("Black_red", 15)):
+        return super().__init__(name, hp, attack_value, defense_value, dice)
+    
+    @classmethod
+    def summon_balrog(cls):
+        print("Cinqs Boss sont morts ! Le Balrog apparait !")
+        return cls()
--- ennemis.py
+++ ennemis.py
@@ -6,11 +6,11 @@ class Zombie(Character):
         super().__init__(name, hp, attack_value, defense_value, dice)
 
     def compute_damages(self, roll, target):
-        print(f"🧟 Zombie attacks you!")
+        print(f"🧟 Zombie Vous attaque !")
         return super().compute_damages(roll, target)
 
     def compute_raw_damages(self, damages, roll, attacker):
-        print(f"🧟 Zombie is weak!")
+        print(f"🧟 Zombie a faim !")
         return super().compute_raw_damages(damages, roll, attacker)
 
     @classmethod
@@ -24,7 +24,8 @@ class Zombie2_0(Zombie):
     
     def compute_raw_damages(self, damages, roll, attacker):
         print(f"🧟 Zombie robuste a faim ! (-2 dmg)" )
-        return super().compute_raw_damages(damages, roll, attacker) - 2
+        raw_damages = super().compute_raw_damages(damages, roll, attacker) - 2
+        return max(0, raw_damages)  # Assure que les dégâts ne peuvent pas être négatifs
     
     @classmethod
     def create_enemy(cls, dice):
@@ -37,7 +38,8 @@ class Zombie_guerrier(Zombie):
     
     def compute_raw_damages(self, damages, roll, attacker):
         print(f"🧟 Zombie guerrier a faim ! (-5 dmg)")
-        return super().compute_raw_damages(damages, roll, attacker) - 5
+        raw_damages = super().compute_raw_damages(damages, roll, attacker) - 5
+        return max(0, raw_damages)
     
     @classmethod
     def create_enemy(cls, dice):
@@ -52,7 +54,7 @@ class Skeletons(Character):
         return super().compute_damages(roll, target) + 3
         
     def compute_raw_damages(self, damages, roll, attacker):
-        print(f"💀 Squelette prends des dégâts")
+        print(f"💀 Squelette prends des dégâts !")
         return super().compute_raw_damages(damages, roll, attacker)
     
     @classmethod
@@ -66,25 +68,30 @@ class Reinforced_Skeleton(Skeletons):
     
     def compute_raw_damages(self, damages, roll, attacker):
         print(f"💀 Squelette renforcé prends des dégâts ! (-3 dmg)")
-        return super().compute_raw_damages(damages, roll, attacker) - 3
+        raw_damages = super().compute_raw_damages(damages, roll, attacker) - 3
+        return max(0, raw_damages)
     
     @classmethod
     def create_enemy(cls, dice):
-        return cls(dice=dice)
+        return cls(name="Squelette renforcé",dice=dice)
 
 class armor_Skeletons(Skeletons):
     def compute_raw_damages(self, damages, roll, attacker):
         print(f"💀 Squelettes à armure prends des dégâts ! (-6 dmg)")
-        return super().compute_raw_damages(damages, roll) - 6
+        raw_damages = super().compute_raw_damages(damages, roll, attacker) - 6
+        return max(0, raw_damages)
     
     @classmethod
     def create_enemy(cls, dice):
-        return cls(dice=dice)
-
+        return cls(name="squelette à armure",dice=dice)
 
 class Goblins(Character):
     def __init__ (self, name="gobelins", hp=20, attack_value=5, defense_value=5, dice=Dice("white_green", 6)):
         super().__init__(name, hp, attack_value, defense_value, dice)
+
+    @classmethod
+    def create_enemy(cls, dice):
+        return cls(dice=dice)
 
 class big_goblins(Goblins):
     def compute_damages(self, roll, target):
@@ -93,11 +100,12 @@ class big_goblins(Goblins):
     
     def compute_raw_damages(self, damages, roll, attacker):
         print(f"👹 Gros gobelin prends des dégâts ! (-4 dmg)")
-        return super().compute_raw_damages(damages, roll, attacker) - 4
+        raw_damages = super().compute_raw_damages(damages, roll, attacker) - 4
+        return max(0, raw_damages)
     
     @classmethod
     def create_enemy(cls, dice):
-        return cls(dice=dice)
+        return cls(name="Gros gobelin",dice=dice)
         
 
 class Trolls(Character):
@@ -115,92 +123,10 @@ class Olog_hai(Trolls):
     
     @classmethod
     def create_enemy(cls, dice):
-        return cls(dice=dice)
+        return cls(name="Olog hai",dice=dice)
 
 
 class Renegas(Character):
     pass
 
-
-class Boss(Character):
-    boss_killed_count = 0
-    def __init__ (self, name="Boss", hp=50, attack_value=20, defense_value=20, dice=Dice("Red", 10)):
-        return super().__init__(name, hp, attack_value, defense_value, dice)
-
-class Cadaverus_Devorator(Boss): #Boss Zombie
-    def compute_damages(self, roll, target):
-        print("Le Cadaverus Devorator vous attaque ! (+5 dmg)")
-        return super().compute_damages(roll, target) + 5
-    
-    def compute_raw_damages(self, damages, roll, attacker):
-        print("Le Cadaverus Devorator prends des dégâts ! (-5 dmg)")
-        return super().compute_raw_damages(damages, roll, attacker) - 5
-
-class Kondylos_o_Sarantapus(Boss): #boss squelette
-    def compute_damages(self, roll, target):
-        print("Le Kondylos_o_Sarantapus vous attaque ! (+6 dmg)")
-        return super().compute_damages(roll, target) + 6
-    
-    def compute_raw_damages(self, damages, roll, attacker):
-        print("Le ondylos_o_Sarantapus prends des dégâts ! (-4 dmg)")
-        return super().compute_raw_damages(damages, roll, attacker) - 4
-    
-class Roi_Gobelin(Boss): #boss gobelin
-    def compute_damages(self, roll, target):
-        print("Le Roi Gobelin vous attaque ! (+7 dmg)")
-        return super().compute_damages(roll, target) + 7
-    
-    def compute_raw_damages(self, damages, roll, attacker):
-        print("Le Roi Gobelin prends des dégâts ! (-7 dmg)")
-        return super().compute_raw_damages(damages, roll, attacker) -7 
-    
-class Garrok_le_Féroce(Boss): #troll boss
-    def compute_damages(self, roll, target):
-        print("Le Garrok le Féroce vous attaque ! (+10 dmg)")
-        return super().compute_damages(roll, target) +10 
-    
-    def compute_raw_damages(self, damages, roll, attacker):
-        print("Le Garrok le Féroce prends des dégâts ! (-10 dmg)")
-        return super().compute_raw_damages(damages, roll, attacker) -10 
-
-    @classmethod
-    def increase_boss_killed_count(cls):
-        cls.boss_killed_count += 1
-        if cls.boss_killed_count % 5 == 0:
-            return Balrog.summon_balrog()
-        else:
-            return None
-
-class Balrog(Character):
-    def __init__ (self, name="Balrog", hp=100, attack_value=50, defense_value=50, dice=Dice("Black_red", 15)):
-        return super().__init__(name, hp, attack_value, defense_value, dice)
-    
-    @classmethod
-    def summon_balrog(cls):
-        print("Cinqs Boss sont morts ! Le Balrog apparait !")
-        return cls()
-    
-class TESTE(Character):
-    def __init__(self, name="TESTE", hp=10000, attack_value=10000, defense_value=10000, dice=Dice("blue", 20)) -> None:
-        super().__init__(name, hp, attack_value, defense_value, dice)
-
-
 ENNEMIES = [Zombie, Zombie2_0, Zombie_guerrier, Skeletons, Reinforced_Skeleton, armor_Skeletons, Goblins, big_goblins, Trolls, Olog_hai]
-
-#char1 = TESTE()
-#char2 = Roi_Gobelin()
-#char3 = Garrok_le_Féroce()
-#char4 = Kondylos_o_Sarantapus()
-#char5 = Cadaverus_Devorator()
-#char6 = Roi_Gobelin()
-
-#while char1.is_alive() and char2.is_alive() and char3.is_alive() and char4.is_alive() and char5.is_alive() and char6.is_alive():
- #   char1.attack(char2)
-  #  char1.attack(char3)
-  #  char1.attack(char4)
-  #  char1.attack(char5)
-  #  char1.attack(char6)
-
-
-
-#okokok
